72ff75ccb6a72333e28a0290a18e01912a3ddb4c
fco1000 <favor.charles9@gmail.com>
2023-09-26 16:49:02 +0300

    security upddates

--- resume_generator/resume_generator/settings.py
+++ resume_generator/resume_generator/settings.py
@@ -148,7 +148,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'fkhaemba02@gmail.com'#THE EMAIL
-EMAIL_HOST_PASSWORD = 'pwgmyoopzjwrqwhn' 
+EMAIL_HOST_USER = 'example@gmail.com'#THE EMAIL
+EMAIL_HOST_PASSWORD = 'password' 
 
-DEFAULT_FROM_EMAIL = 'fkhaemba02@gmail.com'
+DEFAULT_FROM_EMAIL = 'example@gmail.com'
